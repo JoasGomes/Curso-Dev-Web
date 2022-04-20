d407b506b821cb6c4feb51a548d1ba9919555172
JoasGomes <jojoca.vitor777@gmail.com>
2022-04-19 22:18:10 -0300

    aula1

---
+++ curso dev web/secaoPHP7/aula1.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    //ambas tem a mesma função a diferença está na velocidade 
+        echo 'comando echo'
+    //elas mostrando algo na tela bem parecido com o console.log() do js 
+        print 'comando print'
+    ?>
+
+<?php
+        echo 'utilizando a tag padrão';
+    ?>
+
+    <br/>
+
+    <?= 
+        'utilizando a tag impressão';
+    ?>
+
+    <br/>
+
+    <? 
+    echo 'ultilizando a tag curta'
+    ?>
+</body>
+</html>
