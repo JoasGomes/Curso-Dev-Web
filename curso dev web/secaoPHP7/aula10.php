6a4e92ff772cf164d6aef008500e7b1c3ed9ea09
JoasGomes <jojoca.vitor777@gmail.com>
2022-04-25 13:21:56 -0300

    aula10

---
+++ curso dev web/secaoPHP7/aula10.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    
+        //funções para tarefas com matemática
+        /*
+        ceil($numero) -> arredonda para cima
+        floor($numero) -> arredonda para baixo
+        round($numero) -> arredonda com base nas casas decimais
+        rand() -> gera numero aleatório
+        aqrt($numero) -> retorna a raiz quadrada
+        
+        */
+
+        //$num = 7.3;
+
+        //echo ceil($num); //arredonda pra cima
+
+        //echo floor($num); //arredonda pra baixo
+    
+        //echo round($num); //arrendoando com base na fração
+
+        echo rand(10, 20); //gerar um valor aleatório
+
+        echo sqrt(9); //retorna a raiz
+        
+
+
+    
+    
+    ?>
+</body>
+</html>
